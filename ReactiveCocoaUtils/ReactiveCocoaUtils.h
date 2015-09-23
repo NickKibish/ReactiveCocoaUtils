c14aae5c387d25e7169c1ffef2772a4d1eb6a5f0
Nick Kibish <NickKibish@steelkiwi>
2015-09-23 15:07:21 +0300

    first commit

---
+++ ReactiveCocoaUtils/ReactiveCocoaUtils.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveCocoaUtils.h
+//  ReactiveCocoaUtils
+//
+//  Created by Nick Kibish on 23.09.15.
+//  Copyright © 2015 Nick Kibish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveCocoaUtils.
+FOUNDATION_EXPORT double ReactiveCocoaUtilsVersionNumber;
+
+//! Project version string for ReactiveCocoaUtils.
+FOUNDATION_EXPORT const unsigned char ReactiveCocoaUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveCocoaUtils/PublicHeader.h>
+
+
